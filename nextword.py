256de6303a96a452793e2c4fb7205d1e14f2fdf6
JoshuaLeighton1 <joshuaroelf@gmail.com>
2025-05-25 20:01:43 +0200

    preprocessing added

--- nextword.py
+++ nextword.py
@@ -14,8 +14,27 @@ with open('corpus.txt', 'r') as file:
 
 
 #remove punctuation and convert to lowercase 
-text = text.translate(str.maketrans('', '', string(punctuation)))
+text = text.translate(str.maketrans('', '', string.punctuation))
 text = text.lower()
 
 #split into words to tokenize
 words = text.split()
+
+#build vocabulary: map words to unique indices
+vocab = set(words)
+word_to_idx = {word: idx for idx, word in enumerate(vocab)}
+idx_to_word = {idx: word for word, idx in word_to_idx.items()}
+vocab_size = len(vocab)
+#create sequence for training
+
+#number of words in each input sentence
+sequence_length = 5 
+sequences = []
+for i in range(sequence_length, len(words)):
+    seq = words[i-sequence_length:i]
+    target=words[i]
+    sequences.append((seq, target))
+
+
+
+
